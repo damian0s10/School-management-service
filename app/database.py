cad590553293bb7414853ebc8c1302d254731d01
Adam Sawicki <asawicki1996@gmail.com>
2019-12-16 19:34:40 +0100

    Database API refactored, views.py adapted to db API

--- app/database.py
+++ app/database.py
@@ -8,121 +8,111 @@ class Database(object):
         self.database = database
         self.user = userName
         self.password = password
+        self.connection = self.connect()
         
-#This method establish connection to database. 
+    #This method establish connection to database. 
     def connect(self):
-        cnx = connect(user=self.user,
-                      host=self.host,
-                      password=self.password,
-                      database=self.database,
-                      port=self.port,
-                      )
-        return cnx
+        connection = connect(
+            user=self.user,
+            host=self.host,
+            password=self.password,
+            database=self.database,
+            port=self.port,
+            )
+        return connection
+       
+#####################################  USER  ###############################################
 
-#This method adds user to database
-    def add_user(self, user_data):
-        cnx = self.connect()
-        cursor = cnx.cursor()
-        insert_query = "INSERT INTO users(userGId,firstName,lastName, email, pass, user_type, active) VALUES(%s,%s,%s,%s,%s,%s,%s)"
-        cursor.execute(insert_query,
-                      (user_data.userGId,
-                       user_data.firstName,
-                       user_data.lastName,
-                       user_data.email,
-                       user_data.password,
-                       user_data.user_type,
-                       1))
-        cnx.commit()
-        cnx.close()
-#This method adds admin to administrators when name given
-    def change_type(self, user_type, email):
-        cnx = self.connect()
-        cursor = cnx.cursor()
-        get_user_promoted_query="UPDATE users SET user_type=%s WHERE email = %s"
-        cursor.execute(get_user_promoted_query, (user_type, email,))
-        cnx.commit()
-        cnx.close()
+    #######  CREATE USER  #######
 
-    def change_activity(self, value, email):
-        cnx = self.connect()
-        cursor = cnx.cursor()
-        query="UPDATE users SET active=%s WHERE email = %s"
-        cursor.execute(query, (value, email,))
-        cnx.commit()
-        cnx.close()
+    def createUser(self, user_data):
+        cursor = self.connection.cursor()
+        insert_query = '''INSERT INTO
+                            users(
+                                userGId, 
+                                firstName, 
+                                lastName, 
+                                email, 
+                                pass, 
+                                user_type, 
+                                active) 
+                            VALUES(%s,%s,%s,%s,%s,%s,%s)'''
+        cursor.execute(
+            insert_query,
+            (user_data.userGId,
+             user_data.firstName,
+             user_data.lastName,
+             user_data.email,
+             user_data.password,
+             user_data.user_type,
+             1))
+        self.connection.commit()
+        cursor.close()
 
-#This method adds course, only admin can use 
-    def add_course(self, course_data):
-        cnx = self.connect()
-        cursor = cnx.cursor()
-        insert_query = "INSERT INTO subjects(name, description) VALUES(%s,%s)"
-        cursor.execute(insert_query,(course_data.name,course_data.description))
-        cnx.commit()
-        cnx.close()
 
-#This method adds group, only admin can use 
-    def add_group(self, group_data):
-        cnx = self.connect()
-        cursor = cnx.cursor()
-        insert_query = "INSERT INTO groups(active, subjectId, teacherId) VALUES(%s,%s,%s)"
-        cursor.execute(insert_query,(group_data.active,group_data.subjectId,group_data.teacherId))
-        cnx.commit()
-        cnx.close()
+    #######  UPDATE USER  #######
 
-#This method gets all courses, only admin can use 
-    def get_all_courses(self):
-        cnx = self.connect()
-        cursor = cnx.cursor()
-        query = "SELECT subjectId, name,description FROM subjects "
-        cursor.execute(query)
-        courses= cursor.fetchall()
-        if courses:
-            tab = []
-            for course in courses:
-                c = Course(subjectId = course[0], name = course[1], description = course[2])
-                tab.append(c)
-            cnx.close()
-            return tab
-        else:
-            cnx.close()
-            return None
-#This methon gets all teachers, only admin can use
-    def get_all_teachers(self):
-        cnx = self.connect()
-        cursor = cnx.cursor()
-        query ='''SELECT 
-                    userGId,
+    def updateUser(self, user_data, email):
+        cursor = self.connection.cursor()
+        update_query='''UPDATE users
+                        SET 
+                            firstName=%s,
+                            lastName=%s,
+                            email=%s,
+                            pass=%s,
+                            user_type=%s,
+                            active=%s
+                        WHERE 
+                            email=%s'''
+        cursor.execute(
+            update_query,
+            (user_data.firstName,
+             user_data.lastName,
+             user_data.email,
+             user_data.password,
+             user_data.user_type,
+             user_data.active,
+             email))
+        self.connection.commit()
+        cursor.close()
+
+
+    #######  GET USER  #######
+
+    def getUser(self, email = '', userGId = ''):
+        cursor = self.connection.cursor()
+        get_query ='''SELECT userGId,
                     firstName,
                     lastName,
                     email,
                     pass,
                     user_type,
                     active
-                    FROM users WHERE user_type='teacher' AND active = '1' '''
-        cursor.execute(query)
-        teachers = cursor.fetchall()
-        if teachers:
-            tab = []
-            for teacher in teachers:
-                u = User(
-                        userGId = teacher[0],
-                        firstName = teacher[1],
-                        lastName = teacher[2],
-                        email = teacher[3],
-                        password = teacher[4],
-                        user_type = teacher[5],
-                        active = teacher[6])
-                tab.append(u)
-            cnx.close()
-            return tab
-        else:
-            cnx.close()
-            return None
-    
-    
-    def get_members_by_type(self, user_type):
-            cnx = self.connect()
-            cursor = cnx.cursor()
+                  FROM users
+                  WHERE
+                    email= %s OR userGId = %s'''
+        cursor.execute(get_query, (email, userGId,) )
+        results = cursor.fetchone()
+        if results:
+            user = User(
+                userGId = results[0],
+                firstName = results[1],
+                lastName = results[2],
+                email = results[3],
+                password = results[4],
+                user_type = results[5],
+                active = results[6])
+
+            cursor.close()
+            return user
+        cursor.close()
+        return None
+
+
+    #######  GET USERS  #######
+
+    def getUsers(self, user_type, limit=10, index=0):
+            cursor = self.connection.cursor()
             query ='''SELECT 
                         userGId,
                         firstName,
@@ -131,8 +121,8 @@ class Database(object):
                         pass,
                         user_type,
                         active
-                        FROM users WHERE user_type=%s AND active = 1'''
-            cursor.execute(query, (user_type,))
+                        FROM users WHERE user_type=%s AND active=1 LIMIT %s OFFSET %s '''
+            cursor.execute(query, (user_type, limit, index))
             members = cursor.fetchall()
             if members:
                 tab = []
@@ -146,141 +136,123 @@ class Database(object):
                             user_type = member[5],
                             active = member[6])
                     tab.append(u)
-                cnx.close()
+                cursor.close()
                 return tab
             else:
-                cnx.close()
+                cursor.close()
                 return None
 
-    '''
-    def add_student_to_group(self, groupId, userGId):
-        cnx = self.connect()
-        cursor = cnx.cursor()
-        query = "INSERT INTO matches(groupId, studentId) VALUES(%s,%s)"
-        cursor.execute(query, (groupId, studentId,))
-        cnx.commit()
-        cnx.close()
-    '''
 
-    def add_teacher(self, email):
-        cnx = self.connect()
-        cursor = cnx.cursor()
-        query = "UPDATE users SET user_type=teacher WHERE VALUES(%s)"
-        cursor.execute(query, (email,))
-        cnx.commit()
-        cnx.close()
+    #######  DELETE USER  #######
 
+    def deleteUser(self, email):
+        cursor = self.connection.cursor()
+        delete_query='''DELETE FROM
+                            users
+                        WHERE 
+                            email=%s'''
+        cursor.execute(
+            delete_query,
+            (email,))
+        self.connection.commit()
+        cursor.close()
 
-    
-#This method returns admin data when email given
- 
-
-#This method returns user data when name given
-    def get_user_by_email(self,email):
-        cnx = self.connect()
-        cursor = cnx.cursor()
-        query ='''SELECT userGId,
-                    firstName,
-                    lastName,
-                    email,
-                    pass,
-                    user_type,
-                    active
-                  FROM users WHERE email= %s'''
-        cursor.execute(query, (email,) )
-        results = cursor.fetchone()
-        if results:
-            user = User(userGId = results[0],
-                        firstName = results[1],
-                        lastName = results[2],
-                        email = results[3],
-                        password = results[4],
-                        user_type = results[5],
-                        active = results[6])
-            cnx.close()
-            return user
-        
-        cnx.close()
-        return None
+####################################  COURSE  ##############################################
 
 
-    def get_user_by_id(self,userGId):
-        cnx = self.connect()
-        cursor = cnx.cursor()
-        query ='''SELECT userGId,
-                    firstName,
-                    lastName,
-                    email,
-                    pass,
-                    user_type,
-                    active
-                FROM users WHERE userGId= %s'''
-        cursor.execute(query, (userGId,) )
-        results = cursor.fetchone()
-        if results:
-            user = User(userGId = results[0],
-                        firstName = results[1],
-                        lastName = results[2],
-                        email = results[3],
-                        password = results[4],
-                        user_type = results[5],
-                        active = results[5])
-            cnx.close()
-            return user
+    ######### CREATE COURSE ########
+
+    def createCourse(self, course_data):
+        cursor = self.connection.cursor()
+        insert_query = "INSERT INTO subjects(name, description) VALUES(%s,%s)"
+        cursor.execute(insert_query,(course_data.name,course_data.description))
+        self.connection.commit()
+        cursor.close()
+
+
+    ######### GET COURSES ########
+
+    def getCourses(self, limit = 10, index = 0):
+        cursor = self.connection.cursor()
+        get_query = "SELECT subjectId, name,description FROM subjects LIMIT %s OFFSET %s"
+        cursor.execute(get_query,(limit, index))
+        courses= cursor.fetchall()
+        if courses:
+            tab = []
+            for course in courses:
+                c = Course(subjectId = course[0], name = course[1], description = course[2])
+                tab.append(c)
+            cursor.close()
+            return tab
         else:
-            cnx.close()
+            cursor.close()
             return None
 
-    #def get_students_by_group(self, groupId):
-    #    cnx = self.connect()
-    #    cursor = cnx.cursor()
-    #    query = '''SELECT 
-    #                users.firstName,
-    #                users.lastName,
-    #                users.email,
-    #                users.pass,
-    #                users.active,
-    #                students.userId,
-    #                students.studentId
-    #                FROM users INNER JOIN students ON students.userId=users.userId
-    #                INNER JOIN matches ON students.studentId=matches.studentId
-    #                WHERE groupId = %s'''
-    #    cursor.execute(query, (groupId,))
-    #    students = cursor.fetchall()
-    #    if(students):
-    #        tab = []
-    #        for student in students:
-    #            stud = Student(firstName = student[0],
-    #                           lastName = student[1],
-    #                           email = student[2],
-    #                           password = student[3],
-    #                           active = student[4],
-    #                           studentId = student[5],
-    #                           userId = student[6])
-    #            tab.append(stud)
-    #        cnx.close()
-    #        return tab
-    #    else:
-    #        cnx.close()
-    #        return None
 
+    #######  DELETE COURSE  #######
 
-    def close_group(self, groupId):
-        cnx = self.connect()
-        cursor = cnx.cursor()
-        insert_query="UPDATE groups SET active = %s WHERE groupId = %s "
-        cursor.execute(insert_query, (False, groupId,))
-        cnx.commit()
-        cnx.close()
+    def deleteCourse(self, courseId):
+        cursor = self.connection.cursor()
+        delete_query='''DELETE FROM
+                            courses
+                        WHERE 
+                            courseId=%s'''
+        cursor.execute(
+            delete_query,
+            (courseId,))
+        self.connection.commit()
+        cursor.close()
 
-    def get_group_by_subjectid(self, subjectId):
-        cnx = self.connect()
-        cursor = cnx.cursor()
+####################################   GROUP   #########################################
+
+    ######### CREATE GROUP ########
+ 
+    def createGroup(self, group_data):
+        cursor = self.connection.cursor()
+        insert_query = '''INSERT INTO groups
+                            (active,
+                            subjectId,
+                            teacherId)
+                          VALUES(%s,%s,%s)'''
+        cursor.execute(insert_query,
+            (group_data.active, 
+             group_data.subjectId, 
+             group_data.teacherId))
+            
+        self.connection.commit()
+        cursor.close()
+  
+    
+    ######## UPDATE GROUP ########
+
+    def updateGroup(self, group_data, groupId):
+        cursor = self.connection.cursor()
+        update_query='''UPDATE groups
+                        SET
+                            active=%s,
+                            subjectId=%s,
+                            teacherId=%s
+                        WHERE groupId = %s'''
+        cursor.execute(
+             update_query,
+            (group_data.active,
+             group_data.subjectId,
+             group_data.teacherId,
+             groupId,))
+
+        self.connection.commit()
+        cursor.close()
+
+    
+    ######## GET GROUPS ########
+
+    def getGroups(self, subjectId, limit = 10, index = 0):
+        cursor = self.connection.cursor()
         get_query='''SELECT groupId,
                     subjectId,
                     teacherId
-                    FROM groups WHERE subjectId= %s and active=1'''
-        cursor.execute(get_query, (subjectId,) )          
+                    FROM groups WHERE subjectId= %s AND active=1 LIMIT %s OFFSET %s'''
+        cursor.execute(get_query, (subjectId, limit, index) )          
         groups = cursor.fetchall()
         if groups:
             tab = []
@@ -290,21 +262,39 @@ class Database(object):
                         subjectId = group[1],
                         teacherId = group[2])
                 tab.append(g)
-            cnx.close()
+            cursor.close()
             return tab
         else:
-            cnx.close()
+            cursor.close()
             return None
-     
-    def get_lessons_by_groupid(self, groupId):
-        cnx = self.connect()
-        cursor = cnx.cursor()
+
+        #######  DELETE GROUP  #######
+
+    def deleteGroup(self, groupId):
+        cursor = self.connection.cursor()
+        delete_query='''DELETE FROM
+                            groups
+                        WHERE 
+                            groupId=%s'''
+        cursor.execute(
+            delete_query,
+            (groupId,))
+        self.connection.commit()
+        cursor.close()
+
+
+######################################### LESSON ####################################
+
+    ########## GET LESSONS ##########
+
+    def getLessons(self, groupId, limit = 5, index = 0):
+        cursor = self.connection.cursor()
         get_query='''SELECT groupId,
                     classroom,
                     dateValue,
                     timeValue
-                    FROM lessons WHERE groupId= %s'''
-        cursor.execute(get_query, (groupId,) )          
+                    FROM lessons WHERE groupId= %s LIMIT %s OFFSET %s'''
+        cursor.execute(get_query, (groupId, limit, index))        
         lessons = cursor.fetchall()
         if lessons:
             tab = []
@@ -315,8 +305,8 @@ class Database(object):
                         dateValue = lesson[2],
                         timeValue = lesson[3])
                 tab.append(l)
-            cnx.close()
+            cursor.close()
             return tab
         else:
-            cnx.close()
+            cursor.close()
             return None   
